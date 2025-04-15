cbb1dbd2e8d0cbaee714e2c6c2c9a35e6ddbca4e
강인화 <rkddlsghk1998@gmail.com>
2025-04-16 01:26:59 +0900

    feat: 초기 프로젝트 셋업

---
+++ src/main/java/kr/kro/koreatechmap/KmapApiApplication.java
@@ -0,0 +1,13 @@
+package kr.kro.koreatechmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KmapApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KmapApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/kro/koreatechmap/KmapApiApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.kro.koreatechmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KmapApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
